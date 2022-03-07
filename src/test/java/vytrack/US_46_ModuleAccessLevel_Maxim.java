ff645f335959c6522fe98e140d7ecf5fb277d5f5
cherepanovmaxim <cherepanov.maxim91@gmail.com>
2022-03-07 11:07:30 -0500

    add US_46 class

--- src/test/java/vytrack/US_46_ModuleAccessLevel_Maxim.java
+++ src/test/java/vytrack/US_46_ModuleAccessLevel_Maxim.java
@@ -1,2 +1,7 @@
-package vytrack;public class US_46_ModuleAccessLevel_Maxim {
+package vytrack;
+
+public class US_46_ModuleAccessLevel_Maxim {
+    public static void main(String[] args) {
+        System.out.println("check");
+    }
 }
