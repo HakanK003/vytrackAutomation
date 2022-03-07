e5c43b479366f3cb1aa3f3d718f72241efde7af3
cherepanovmaxim <cherepanov.maxim91@gmail.com>
2022-03-07 18:17:24 -0500

    update VytrackUtils. Add testCases folder

--- src/test/java/vytrack/US_46_ModuleAccessLevel_Maxim.java
+++
@@ -1,7 +0,0 @@
-package vytrack;
-
-public class US_46_ModuleAccessLevel_Maxim {
-    public static void main(String[] args) {
-        System.out.println("check");
-    }
-}
---
+++ src/test/java/vytrack/testCases/US_46_ModuleAccessLevel_Maxim.java
@@ -0,0 +1,19 @@
+package vytrack.testCases;
+
+import org.openqa.selenium.WebDriver;
+import vytrack.utilities.Driver;
+import vytrack.utilities.VytrackUtils;
+
+public class US_46_ModuleAccessLevel_Maxim {
+    public static void main(String[] args) {
+
+
+        VytrackUtils.loginAsDriver();
+
+
+        WebDriver driver = Driver.getDriver();
+
+
+
+    }
+}
