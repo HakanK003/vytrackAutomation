24017f7806c976dbe950de8e56f9f56019565e77
Alena <leksax@gmail.com>
2022-03-14 20:23:44 -0400

    Automation in process

--- src/test/java/vytrack/testCases/US27_OdometerPage_Alena.java
+++ src/test/java/vytrack/testCases/US27_OdometerPage_Alena.java
@@ -5,23 +5,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import vytrack.tests.base.TestBase;
 import vytrack.utilities.BrowserUtils;
 import vytrack.utilities.Driver;
 import vytrack.utilities.VytrackUtils;
 
-public class AlenaUS27_OdometerPage extends TestBase {
+public class US27_OdometerPage_Alena extends TestBase {
 
-    @Test
-    public static void noManagersAccess(){
+    @DataProvider(name="dataForTest1")
+    public Object[][] provideData(){
+        return new Object[][]{
+
+                {"storemanager100"},
+                {"salesmanager270"},
+
+                {"storemanager99"},
+                {"salesmanager267"},
+        };
+    }
+
+    @Test(dataProvider = "dataForTest1")
+    public static void noManagersAccess(String username){
         //# Truck Drivers:   user 150        user155
         //# Store Managers: storemanager99   storemanager100
         //# Sales Managers:  salesmanager267   salesmanager270
 
         Actions actions = new Actions(Driver.getDriver());
 
-        VytrackUtils.loginWithUsername("storemanager99");
+        VytrackUtils.loginWithUsername(username);
         BrowserUtils.sleep(3);
 
         WebElement fleetTab = Driver.getDriver().findElement(By.xpath("//li[@class='dropdown dropdown-level-1'][1]"));
@@ -40,12 +53,29 @@ public class AlenaUS27_OdometerPage extends TestBase {
         Assert.assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    @Test
-    public static void driverAccess(){
+
+
+    @DataProvider(name = "truckDriverLogin")
+    public Object[][] driverData() {
+        return new Object[][]{
+
+                {"user150", "UserUser123"},
+                {"user151", "UserUser123"},
+                {"user152", "UserUser123"},
+                {"user153", "UserUser123"},
+                {"user154", "UserUser123"},
+                {"user155", "UserUser123"},
+
+
+        };
+    }
+
+        @Test(dataProvider = "truckDriverLogin")
+     public static void driverAccess(String user, String password){
 
         Actions actions = new Actions(Driver.getDriver());
 
-        VytrackUtils.loginWithUsername("user155");
+        VytrackUtils.loginWithUsername(user);
         BrowserUtils.sleep(3);
 
         WebElement fleetTab = Driver.getDriver().findElement(By.xpath("//li[@class='dropdown dropdown-level-1'][1]"));
